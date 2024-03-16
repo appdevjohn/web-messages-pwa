a5b97d78288ff2be9e674c524329f9c9506b5bce
John Champion <johnt.champion@gmail.com>
2024-03-16 10:51:25 -0400

    basic integration with backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,106 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { MessageType } from './types'
+import socket from './util/socket'
 import MessageView from './components/MessageView'
 import ComposeBox from './components/ComposeBox'
 
+const CONVO_ID = 17
+
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected)
   const [messages, setMessages] = useState<MessageType[]>([])
+  const [sendingMessage, setSendingMessage] = useState<MessageType>()
+
+  // Handle WebSocket events.
+  useEffect(() => {
+    const onConnect = () => setIsConnected(true)
+    const onDisconnect = () => setIsConnected(false)
+    const onMessages = (msgs: any[]) => {
+      const parsedMessages: MessageType[] = msgs.map((msg) => ({
+        id: msg['id'],
+        userId: 'TODO',
+        timestamp: msg['createdAt'],
+        content: msg['content'],
+        type: msg['type'],
+        userProfilePic: '',
+        userFullName: 'TODO',
+        delivered: 'delivered',
+      }))
+      setMessages(parsedMessages)
+    }
+    const onResponse = (payload: any) => {
+      const eventType = payload['event']
+      switch (eventType) {
+        case 'send-message':
+          break
+        case 'create-conversation':
+          break
+        case 'delete-conversation':
+          break
+
+        default:
+          break
+      }
+    }
+    const onUpdate = (newMessage: any) => {
+      setMessages((msgs) => {
+        const messagesCopy = JSON.parse(JSON.stringify(msgs))
+        messagesCopy.push({
+          id: newMessage['id'],
+          userId: 'TODO',
+          timestamp: newMessage['createdAt'],
+          content: newMessage['content'],
+          type: newMessage['type'],
+          userProfilePic: '',
+          userFullName: 'TODO',
+          delivered: 'delivered',
+        })
+        return messagesCopy
+      })
+    }
+
+    socket.on('connect', onConnect)
+    socket.on('disconnect', onDisconnect)
+    socket.on('messages', onMessages)
+    socket.on('response', onResponse)
+    socket.on(`${CONVO_ID}`, onUpdate)
+
+    return () => {
+      socket.off('connect', onConnect)
+      socket.off('disconnect', onDisconnect)
+      socket.off('messages', onMessages)
+      socket.off('response', onResponse)
+      socket.off(`${CONVO_ID}`, onUpdate)
+    }
+  }, [])
+
+  // Fetch messages when connected.
+  useEffect(() => {
+    socket.emit('get-messages', { convoId: CONVO_ID })
+  }, [isConnected])
+
+  // Send messages as they appear in the transcript.
+  useEffect(() => {
+    if (sendingMessage) {
+      socket.emit('send-message', {
+        convoId: CONVO_ID,
+        content: sendingMessage.content,
+      })
+      setSendingMessage(undefined)
+    }
+  }, [sendingMessage])
 
   const sendMessageHandler = (messageContent: string) => {
-    setMessages((prevMessages) => {
-      const copy = prevMessages.map((m) => ({ ...m }))
-      return [
-        ...copy,
-        {
-          content: messageContent,
-          delivered: 'delivered',
-          timestamp: new Date(),
-          type: 'text',
-          userFullName: 'John Champion',
-          userProfilePic: '',
-          userId: '1',
-          id: String(prevMessages.length),
-        },
-      ]
+    setSendingMessage({
+      content: messageContent,
+      delivered: 'not-delivered',
+      timestamp: new Date(),
+      type: 'text',
+      userFullName: 'TODO',
+      userProfilePic: '',
+      userId: 'TODO',
+      id: Math.random().toString(),
     })
   }
 
@@ -34,7 +112,7 @@ function App() {
           isLoadingOlderMessages={false}
           onLoadOlderMessages={() => {}}
           showLoadOlderMessagesButton={false}
-          messages={messages}
+          messages={sendingMessage ? [...messages, sendingMessage] : messages}
         />
         <ComposeBox
           becameActive={() => {}}
