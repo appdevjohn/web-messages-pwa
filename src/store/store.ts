795033b487177c02280f88a9790cfa819d46c83c
John Champion <johnt.champion@gmail.com>
2025-10-30 10:02:22 -0400

    Handle token refresh in Axios instance

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/auth'
+import { attachStore } from '../util/rest'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
   },
 })
+
+attachStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
