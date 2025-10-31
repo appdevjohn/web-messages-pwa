40e8c7ba7ac03e66a6552af00f4914dc21d25590
John Champion <johnt.champion@gmail.com>
2025-10-30 23:23:02 -0400

    update socket client to update access token if necessary

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/auth'
-import { attachStore } from '../util/rest'
+import { attachStore as attachRestStore } from '../util/rest'
+import { attachStore as attachSocketStore } from '../util/socket'
 
 export const store = configureStore({
   reducer: {
@@ -8,7 +9,8 @@ export const store = configureStore({
   },
 })
 
-attachStore(store)
+attachRestStore(store)
+attachSocketStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
